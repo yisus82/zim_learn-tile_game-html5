e59ab6f8ceb352a382a94ee509f9cbbec4976674
yisus82 <djalma8@gmail.com>
2024-12-08 14:18:10 +0100

    Add plasma pods

--- js/game.js
+++ js/game.js
@@ -1,4 +1,29 @@
-const ready = () => {};
+const assets = ['plasmapods.jpg'];
+const path = 'https://zimjs.org/assets/';
+
+const ready = () => {
+  // Pods
+  const pod = new Sprite({
+    image: 'plasmapods.jpg',
+    cols: 10,
+    rows: 10,
+  }).reg(CENTER);
+  const cols = 4;
+  const rows = 5;
+  const pods = new Tile({
+    obj: pod,
+    cols,
+    rows,
+    spacingH: 10,
+    spacingV: 10,
+  })
+    .scaleTo(S, 95, 95)
+    .center();
+  // loop through all the pods and set its frame to a random number
+  pods.loop(pod => {
+    pod.frame = rand(99);
+  });
+};
 
 new Frame({
   scaling: FIT,
@@ -7,4 +32,6 @@ new Frame({
   color: black,
   outerColor: darker,
   ready,
+  assets,
+  path,
 });
