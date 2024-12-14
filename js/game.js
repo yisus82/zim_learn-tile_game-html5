bebbd4545cae1ea2265c2421ee449fa0c0a89c56
yisus82 <djalma8@gmail.com>
2024-12-14 22:40:55 +0100

    Advance levels

--- js/game.js
+++ js/game.js
@@ -2,7 +2,7 @@
 const assets = ['plasmapods.jpg'];
 const path = 'https://zimjs.org/assets/';
 
-// Level sizes
+// Game variables
 const levelSizes = [
   [4, 5],
   [5, 7],
@@ -11,7 +11,18 @@ const levelSizes = [
   [8, 11],
 ];
 
-const makeLevel = (currentLevel = 0) => {
+/**
+ * Make a level
+ *
+ * @param {number} level The level to make. Defaults to 0.
+ */
+const makeLevel = (level = 0) => {
+  // Check available levels
+  if (level >= levelSizes.length) {
+    // We have no more levels, so do something
+    return;
+  }
+
   // Pods sprite sheet
   const pod = new Sprite({
     image: 'plasmapods.jpg',
@@ -20,7 +31,7 @@ const makeLevel = (currentLevel = 0) => {
   }).reg(CENTER);
 
   // Create the pods grid
-  const [cols, rows] = levelSizes[currentLevel];
+  const [cols, rows] = levelSizes[level];
   const pods = new Tile({
     obj: pod,
     cols,
@@ -32,6 +43,21 @@ const makeLevel = (currentLevel = 0) => {
     .center()
     .cur();
 
+  // Create the rings container
+  const rings = new Container({
+    width: W,
+    height: H,
+  }).addTo();
+
+  // Animate the pods in
+  pods.alp(0).animate({
+    wait: level === 0 ? 0 : 1,
+    props: {
+      alpha: 1,
+    },
+    time: 0.5,
+  });
+
   // Create the frame options
   const options = [];
   loop(100, i => {
@@ -42,7 +68,7 @@ const makeLevel = (currentLevel = 0) => {
   shuffle(options);
 
   // Take some frames out of the options to use as eternals
-  const numEternals = currentLevel + 2;
+  const numEternals = level + 2;
   const eternalsFrames = options.splice(0, numEternals);
 
   // Create all available tile indexes
@@ -57,36 +83,8 @@ const makeLevel = (currentLevel = 0) => {
   // Create a set to store the correct pods
   const correctPods = new Set();
 
-  // Mouse down event to select pods
-  pods.on('mousedown', event => {
-    if (eternalsTileIndexes.includes(event.target.tileNum)) {
-      // Outline the selected pod
-      // We have to scale the circles because they are global, not inside scaled tile
-      new Circle({
-        radius: (event.target.width / 2) * pods.scale,
-        color: clear,
-        borderColor: white,
-        borderWidth: 18,
-        dashed: true,
-      }).loc(event.target);
-
-      // Add the pod to the correct pods set
-      correctPods.add(event.target);
-
-      // If we have selected all the correct pods, do something
-      if (correctPods.size === numEternals) {
-        pods.loop(pod => {
-          pod.removeFrom();
-        }, true);
-      }
-    } else {
-      // Just do something for now...
-      event.target.sca(0.5);
-    }
-  });
-
   // Change pods positions every second
-  interval({
+  const podsInterval = interval({
     // Time in seconds
     time: 1,
     immediate: true,
@@ -100,6 +98,63 @@ const makeLevel = (currentLevel = 0) => {
       });
       S.update();
     },
+  });
+
+  // Mouse down event to select pods
+  pods.on('mousedown', event => {
+    if (eternalsTileIndexes.includes(event.target.tileNum)) {
+      // Outline the selected pod
+      // We have to scale the circles because they are global, not inside scaled tile
+      new Circle({
+        radius: (event.target.width / 2) * pods.scale,
+        color: clear,
+        borderColor: white,
+        borderWidth: 18,
+        dashed: true,
+      }).loc({
+        target: event.target,
+        container: rings,
+      });
+
+      // Add the pod to the correct pods set
+      correctPods.add(event.target);
+
+      // If we have selected all the correct pods, do something
+      if (correctPods.size === numEternals) {
+        // Animate out the previous pods
+        // Caching makes animating smoother on mobile
+        pods.cache().animate({
+          props: {
+            alpha: 0,
+          },
+          time: 0.5,
+          call: target => {
+            target.dispose();
+          },
+        });
+
+        // Clear the current interval
+        podsInterval.clear();
+
+        // Clear the rings
+        rings.top().animate({
+          rewind: true,
+          time: 0.5,
+          props: {
+            alpha: 0,
+          },
+          rewindCall: () => {
+            rings.removeAllChildren();
+          },
+        });
+
+        // Advance to the next level
+        makeLevel(level + 1);
+      }
+    } else {
+      // Just do something for now...
+      event.target.sca(0.5);
+    }
   });
 };
 
